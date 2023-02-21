2563524bd9f31180f6b31da93f8fd67254ccf356
Nilu kumari <you@example.com>
2023-02-21 19:21:20 +0530

    first commit

---
+++ src/test/java/pages/HomePage.java
@@ -0,0 +1,108 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utils.BrowserManager;
+
+public class HomePage {
+
+    private WebDriver driver;
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver,this);
+    }
+
+    @FindBy(xpath = "//button[@class='_2KpZ6l _2doB4z']")
+    WebElement click;
+
+    public WebElement getClick() {
+        return click;
+    }
+
+    public WebElement getSearchResult() {
+        return SearchResult;
+    }
+
+    public WebElement getSearchBox() {
+        return searchBox;
+    }
+
+    @FindBy(xpath = "//input[@class='_3704LK']")
+    WebElement searchBox;
+
+    @FindBy(xpath = "//button[@class='L0Z3Pu']")
+    WebElement ClickOn;
+
+    public WebElement getClickOn() {
+        return ClickOn;
+    }
+
+    @FindBy(xpath = "//span[@class=\"_10Ermr\"]//span")
+    WebElement SearchResult;
+
+    @FindBy(xpath = "//div[text()='Price -- Low to High']")
+    WebElement ClickInfo;
+
+    public WebElement getClickInfo() {
+        return ClickInfo;
+    }
+
+    @FindBy(xpath = "//span[@class=\"_10Ermr\"]//span")
+    WebElement result;
+
+    public WebElement getResult() {
+        return result;
+    }
+
+    @FindBy(xpath = "//div[text()='Infinix HOT 20 Play (Fantasy Purple, 64 GB)']")
+    WebElement clickScreen;
+
+    public WebElement getClickScreen() {
+        return clickScreen;
+    }
+
+    public WebElement getDisplayed() {
+        return displayed;
+    }
+
+    @FindBy(xpath ="(//div[@class=\"_1YokD2 _2GoDe3\"])[1]")
+    WebElement displayed;
+
+    @FindBy(xpath = "//div[text()='Price -- High to Low']")
+    WebElement tap;
+
+    public WebElement getTap() {
+        return tap;
+    }
+
+    public WebElement getDisplay() {
+        return display;
+    }
+
+    @FindBy(xpath = "//span[@class=\"_10Ermr\"]//span")
+    WebElement display;
+
+    @FindBy(xpath = "//button[@class='_2KpZ6l _2doB4z']")
+    WebElement clack;
+
+    public WebElement getClack() {
+        return clack;
+    }
+
+    @FindBy(xpath = "//input[@class='_3704LK']")
+    WebElement searchbar;
+
+    public WebElement getSearchbar() {
+        return searchbar;
+    }
+
+    @FindBy(xpath = "//button [@class='L0Z3Pu']")
+    WebElement output;
+
+    public WebElement getOutput() {
+        return output;
+    }
+}
---
+++ src/test/java/steps/StepDefinitions.java
@@ -0,0 +1,116 @@
+package steps;
+
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import pages.HomePage;
+import utils.BrowserManager;
+import utils.QaProps;
+import utils.TestReader;
+
+import java.util.HashMap;
+
+public class StepDefinitions {
+
+    private WebDriver driver;
+    String url;
+    HomePage homePage;
+    HashMap<String, String> data;
+    Scenario scenario;
+
+
+    public StepDefinitions(BrowserManager browserManager){
+        this.driver = browserManager.getDriver();
+    }
+
+    @Before(order = 1)public void before(Scenario scenario){    this.scenario = scenario;}
+    @Given("the user navigates to the home page")
+    public void the_user_navigates_to_the_home_page() {
+        url = QaProps.getValue("url");
+        driver.get(url);
+        data =  TestReader.getData(scenario.getName());
+
+
+
+
+    }
+    @When("the user enter the product name")
+    public void the_user_enter_the_product_name() throws  InterruptedException {
+       homePage = new HomePage(driver);
+       Thread.sleep(2000);
+       homePage.getClick().click();
+        homePage.getSearchBox().sendKeys(data.get("testvalue"));
+        homePage.getClickOn().click();
+
+
+    }
+    @Then("the product result should be displayed")
+    public void the_product_result_should_be_displayed() {
+
+        String text = homePage.getSearchResult().getText();
+        Assert.assertEquals(text,"mobiles");
+    }
+
+
+
+    @And("the user should display the page")
+    public void theUserShouldDisplayThePage() {
+        homePage.getClickInfo().click();
+
+    }
+    @Then("user should able to sort the product by price")
+    public void userShouldAbleToSortTheProductByPrice() {
+        String text = homePage.getResult().getText();
+        Assert.assertEquals(text,"mobiles");
+
+    }
+
+
+    @And("the user see the page")
+    public void theUserSeeThePage() {
+        homePage.getClickScreen().click();
+    }
+
+    @Then("the product open should be displayed")
+    public void theProductOpenShouldBeDisplayed() {
+        WebElement text = homePage.getDisplayed();
+        Assert.assertTrue(text.isDisplayed());
+
+
+    }
+    @And("the user should see the page")
+    public void theUserShouldSeeThePage() {
+        homePage.getTap().click();
+    }
+
+
+    @Then("user should able to sort the product by price high to low")
+    public void userShouldAbleToSortTheProductByPriceHighToLow() {
+        String text = homePage.getDisplay().getText();
+        Assert.assertEquals(text,"mobiles");
+    }
+
+    @When("the user  write the product name")
+    public void theUserEnterWriteTheProductName() {
+        homePage = new HomePage(driver);
+        homePage.getClack().click();
+        homePage.getSearchbar().sendKeys("xyz2234bh");
+
+    }
+
+
+    @Then("the result should displayed")
+    public void theResultShouldDisplayed() {
+        homePage.getOutput().click();
+
+    }
+
+
+}
